2e84004512024d669ab3ccbc07ca1f2a9ef966b3
alido8592 <alido@ualberta.ca>
2017-10-22 06:32:05 -0600

    HabitTest outlined

---
+++ app/src/androidTest/java/com/example/habitup/HabitTest.java
@@ -0,0 +1,28 @@
+package com.example.habitup;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.habitup.Model.Habit;
+import com.example.habitup.Model.UserAccount;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertFalse;
+
+public class HabitTest {
+
+    //testSetHabitNameSuccess()
+    //testSetHabitNameErr()
+    //testHabitNameLength()
+    //testSetReason()
+    //testSetReasonLength
+    //testSetSchedule()
+    //testSetScheduleErr()
+    //testSetAttribute()
+    //testSetAttributeErr()
+    //testAddHabitEvent()
+    //testRemoveHabitEvent()
+
+}
