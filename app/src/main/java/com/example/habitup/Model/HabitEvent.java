7b0e2ae92719f200c2deb99cbe6ad19af7d8b14c
acsyl <shuyang2@ualberta.ca>
2017-10-22 17:23:06 -0600

    fixed problems

--- app/src/main/java/com/example/habitup/Model/HabitEvent.java
+++ app/src/main/java/com/example/habitup/Model/HabitEvent.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 
@@ -13,18 +14,37 @@ public class HabitEvent {
     private GregorianCalendar completedate;
     private Map location;
     private Bitmap Image;
+    private String pathofimage;
     private String comment;
     private byte[] ImageByteArray;
     private int ByteCount;
+    private Habit habit;
 
-    public void setCompletedate(GregorianCalendar completedate){
+
+    public HabitEvent (GregorianCalendar completedate, Map location, String image, String comment,Habit habit){
+        this.comment = comment;
+        this.completedate = completedate;
+        this.location = location;
+        this.comment = comment;
+        this.pathofimage = image;
+        this.habit = habit;
+    }
+
+    public void setCompletedate (GregorianCalendar completedate) throws IllegalArgumentException {
         this.completedate = completedate;
 
     }
     public void setLocation(Map location){
         this.location =  location;
     }
-    public void setImage(String pathOfImage){
+
+    public void setPathofimage(String pathofimage){
+        this.pathofimage = pathofimage;
+        setImage(pathofimage);
+
+    }
+
+    private void setImage(String pathOfImage){
         Image= BitmapFactory.decodeFile(pathOfImage);
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         Image.compress(Bitmap.CompressFormat.JPEG, 100, stream);
@@ -44,7 +64,7 @@ public class HabitEvent {
         bp.compress(Bitmap.CompressFormat.JPEG, 70, stream);
     }
 
-    public void setComment(String comment){
+    public void setComment (String comment) throws IllegalArgumentException {
         this.comment = comment;
     }
 
--- app/src/test/java/com/example/habitup/HabitEventTest.java
+++ app/src/test/java/com/example/habitup/HabitEventTest.java
@@ -1,5 +1,6 @@
 package com.example.habitup;
 
+import com.example.habitup.Model.Habit;
 import com.example.habitup.Model.HabitEvent;
 import com.example.habitup.Model.Map;
 
@@ -16,27 +17,56 @@ import static org.junit.Assert.*;
  */
 public class HabitEventTest {
     @Test
-    public void testHabitEventSetterGetter(){
-        HabitEvent event1= new HabitEvent();
-        event1.setCompletedate(new GregorianCalendar(2017,10,2,15,16,17));
+    public void testHabitEventSetterGetter() {
+        Habit h = new Habit();
+        HabitEvent event1 = new HabitEvent(null,null,null,null,h);
+        event1.setCompletedate(new GregorianCalendar(2017, 10, 2, 15, 16, 17));
         event1.setLocation(new Map());
-        event1.setImage("/Users/abcdefg");
+        event1.setPathofimage("/Users/abcdefg");
         event1.setComment("abcdef");
-        try{
-            GregorianCalendar date1 = new GregorianCalendar(2017,10,2,15,16,17);
+        try {
+            GregorianCalendar date1 = new GregorianCalendar(2017, 10, 2, 15, 16, 17);
             Map location1 = new Map();
             assertTrue(event1.getLocation().equals(location1));
-            assertTrue((event1.getCompletedate()).compareTo(date1) ==0);
-            assertTrue(event1.getImage()!= null);
+            assertTrue((event1.getCompletedate()).compareTo(date1) == 0);
+            assertTrue(event1.getImage() != null);
             assertTrue(event1.getComment().equals("abcdef"));
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             fail();
         }
+    }
 
+
+    @Test
+    public void testCommentLimited(){
+        String c1 = "saasdfaskfdasfasfsafsafasfavsadfsdfsa";
+        Habit h = new Habit();
+        try {
+            HabitEvent event1 = new HabitEvent(null,null,null,c1,h);
+        } catch (IllegalArgumentException e){
+            assertTrue(Boolean.TRUE);
+        }
+
+
+    }
+    @Test
+    public void testCompleteDateRestrict(){
+        Habit h = new Habit();
+        GregorianCalendar date1 = new GregorianCalendar(2016, 10, 2, 15, 16, 17);
+        try {
+            HabitEvent event2 = new HabitEvent(date1,null,null,null,h);
+        } catch (IllegalArgumentException e){
+            assertTrue(Boolean.TRUE);
+        }
+
+    }
 
 
 
     }
 
-}
+
+
+
+
+
