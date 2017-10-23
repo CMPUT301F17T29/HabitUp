846c7ac315d0e9caff9dd4843018a294326513e2
tyheise <tyler.heise@gmail.com>
2017-10-22 19:55:04 -0600

    added a comment

---
+++ app/src/main/java/com/example/habitup/Model/UserAccount.java
@@ -0,0 +1,14 @@
+package com.example.habitup.Model;
+
+import android.media.Image;
+
+/**
+ * Created by Ty on 2017-10-22.
+ */
+
+public class UserAccount {
+
+    public UserAccount(String u, String n, Image p){
+
+    }
+}
