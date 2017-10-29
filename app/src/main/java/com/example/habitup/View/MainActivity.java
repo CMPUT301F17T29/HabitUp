4b1db71d05b364110c8587b6c192e458d8001d31
sharibarboza <barbozashari@gmail.com>
2017-10-29 16:56:46 -0600

    Implement BaseActivity

--- app/src/main/java/com/example/habitup/View/MainActivity.java
+++ app/src/main/java/com/example/habitup/View/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,10 +19,7 @@ import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class MainActivity extends AppCompatActivity {
-    private DrawerLayout drawerLayout;
-    private Toolbar toolbar;
-    ProgressBar progressBar;
+public class MainActivity extends BaseActivity {
 
     private ArrayList<Habit> habitList;
     private ListView habitListView;
@@ -35,10 +30,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        initNavigationDrawer();
-
         // Initialize list view for today's habits
         habitListView = (ListView) findViewById(R.id.habit_listview);
         LayoutInflater inflater = this.getLayoutInflater();
@@ -46,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         habitListView.addHeaderView(profileView);
 
         // Set progress bar
-        progressBar = (ProgressBar) profileView.findViewById(R.id.progress_bar);
+        ProgressBar progressBar = (ProgressBar) profileView.findViewById(R.id.progress_bar);
         progressBar.setProgress(50);
         progressBar.setMax(100);
 
@@ -69,65 +60,5 @@ public class MainActivity extends AppCompatActivity {
             TextView subHeading = (TextView) findViewById(R.id.today_subheading);
             subHeading.setText(getString(R.string.no_habits));
         }
-    }
-
-    public void initNavigationDrawer() {
-        // Taken from https://www.learn2crack.com/2016/03/android-material-design-sliding-navigation-drawer.html
-        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                int id = menuItem.getItemId();
-
-                switch (id){
-                    case R.id.profile:
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.habits:
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.events:
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.friends:
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.requests:
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.finds:
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.map:
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.logout:
-                        drawerLayout.closeDrawers();
-                        break;
-                }
-                return true;
-            }
-        });
-        View header = navigationView.getHeaderView(0);
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
-
-        // Highlight profile list item
-        navigationView.getMenu().getItem(0).setChecked(true);
-
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
-
-            @Override
-            public void onDrawerClosed(View v){
-                super.onDrawerClosed(v);
-            }
-
-            @Override
-            public void onDrawerOpened(View v) {
-                super.onDrawerOpened(v);
-            }
-        };
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
     }
 }
