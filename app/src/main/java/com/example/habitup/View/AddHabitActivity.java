a69998f989ea5736d478ab79dcd00fa40c780e1e
sharibarboza <barbozashari@gmail.com>
2017-10-29 16:32:29 -0600

    Implement BaseActivity class, but include back button for add habit layout

--- app/src/main/java/com/example/habitup/View/AddHabitActivity.java
+++ app/src/main/java/com/example/habitup/View/AddHabitActivity.java
@@ -1,11 +1,13 @@
 package com.example.habitup.View;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import com.example.habitup.Model.Attributes;
@@ -16,10 +18,19 @@ import java.util.Date;
 
 public class AddHabitActivity extends AppCompatActivity {
 
+    ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_habit);
+
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        LayoutInflater inflater = getLayoutInflater();
+        LinearLayout container = (LinearLayout) findViewById(R.id.content_frame);
+        inflater.inflate(R.layout.activity_main, container);
 
         Date currentDate = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy");
@@ -37,5 +48,16 @@ public class AddHabitActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, entries);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         attrSpinner.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
     }
 }
