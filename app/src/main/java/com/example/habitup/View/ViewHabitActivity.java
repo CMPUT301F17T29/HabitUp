bbf7a9f903853f21b309addb789d347365857da5
acsyl <shuyang2@ualberta.ca>
2017-10-29 16:26:19 -0600

    add bar

--- app/src/main/java/com/example/habitup/View/ViewHabitActivity.java
+++ app/src/main/java/com/example/habitup/View/ViewHabitActivity.java
@@ -1,8 +1,11 @@
 package com.example.habitup.View;
 
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.example.habitup.R;
 
@@ -11,8 +14,13 @@ public class ViewHabitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.habits);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setTitle("HabitUp");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+
 
     }
 }
