6d63cb96514d10e199dbe30b1c8bce3ef48cfb93
acsyl <shuyang2@ualberta.ca>
2017-11-05 11:51:32 -0700

    requests

--- app/src/main/java/com/example/habitup/View/FollowActivity.java
+++ app/src/main/java/com/example/habitup/View/FollowActivity.java
@@ -1,6 +1,33 @@
 package com.example.habitup.View;
 
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class FollowActivity {
+
+
+import com.example.habitup.R;
+
+import java.util.ArrayList;
+
+public class FollowActivity extends AppCompatActivity{
+    private ArrayList requestList = new ArrayList <>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.follow_request);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        FollowRequestAdapter adapter = new FollowRequestAdapter(requestList);
+        recyclerView.setAdapter(adapter);
+
+
+
+    }
+
 }
