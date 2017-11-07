5eb9ef83b0434f9d5f03fdfd5b8c5e0c4e518940
sharibarboza <barbozashari@gmail.com>
2017-11-06 18:29:55 -0700

    Change font size and text color in Add Habit layout

--- app/src/main/java/com/example/habitup/View/ViewHabitActivity.java
+++ app/src/main/java/com/example/habitup/View/ViewHabitActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
