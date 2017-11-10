7ee21425332af0c85e15bd9d937e1287085f1675
sharibarboza <barbozashari@gmail.com>
2017-11-09 19:37:27 -0700

    Finish add habit event layout

--- app/src/main/java/com/example/habitup/View/AddHabitEventActivity.java
+++ app/src/main/java/com/example/habitup/View/AddHabitEventActivity.java
@@ -1,6 +1,189 @@
 package com.example.habitup.View;
 
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class AddHabitEventActivity {
+import com.example.habitup.Controller.HabitUpController;
+import com.example.habitup.Model.Habit;
+import com.example.habitup.R;
+
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class AddHabitEventActivity extends AppCompatActivity {
+
+    // Event completion date
+    private int year_x, month_x, day_x;
+    private static final int DIALOG_ID = 1;
+
+    // Clickable image
+    private static final int REQUEST_CODE = 1;
+    Bitmap imageBitMap;
+    Button imageButton;
+    ImageView image;
+
+    Button saveButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+
+        // Set back button
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Get current date
+        final Calendar cal = Calendar.getInstance(Locale.CANADA);
+        year_x = cal.get(Calendar.YEAR);
+        month_x = cal.get(Calendar.MONTH);
+        day_x = cal.get(Calendar.DAY_OF_MONTH);
+
+        // Get date click button
+        ImageView dateClicker = (ImageView) findViewById(R.id.event_date_button);
+
+        // Set selected date
+        setDateString();
+
+        // Set up spinner
+        Spinner habitSpinner = (Spinner) findViewById(R.id.event_habit_spinner);
+
+        // Set up habit types list
+        ArrayList<String> habitNames = new ArrayList<>();
+
+        // TODO: Retrieve habits from current user's HabitList
+        ArrayList<Habit> habitList = new ArrayList<Habit>();
+        for (Habit habit : habitList) {
+            habitNames.add(habit.getHabitName());
+        }
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, habitNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        habitSpinner.setAdapter(adapter);
+
+        // Get location checkbox
+        Switch locationSwitch = (Switch) findViewById(R.id.location_switch);
+
+        // Get photo icon
+        imageButton = (Button) findViewById(R.id.photo_icon);
+        image = (ImageView) findViewById(R.id.taken_image);
+
+        // Allow user to take or choose photo when clicking the photo icon
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (photoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(photoIntent, REQUEST_CODE);
+                }
+            }
+        });
+
+        // Open the date picker dialog
+        dateClicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DIALOG_ID);
+            }
+        });
+
+        // Save button
+        saveButton = (Button) findViewById(R.id.save_event);
+    }
+
+    /**
+     * Opening the date picker dialog
+     * @param id the dialog id
+     * @return the dialog
+     */
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_ID) {
+            return new DatePickerDialog(this, datePicker, year_x, month_x, day_x);
+        }
+        return null;
+    }
+
+    /**
+     * Includes activity for taking picture
+     * @param requestCode the request code for some activity
+     * @param resultCode the result code of the activity
+     * @param data data from the activity
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitMap = (Bitmap) extras.get("data");
+
+            // TODO: Resize image for to appropriate byte size
+            image.setImageBitmap(imageBitMap);
+            image.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Listens for when the user clicks on the back button
+     * @param menuItem the item in the menu
+     * @return true if the item was selected
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
+    }
+
+    /**
+     * Updates the date string in the date text view
+     */
+    private void setDateString() {
+        TextView dateView = (TextView) findViewById(R.id.event_date_text);
+
+        String monthName = new DateFormatSymbols().getShortMonths()[month_x];
+        String dateString = (monthName) + " " + day_x + ", " + year_x;
+        dateView.setText(dateString);
+    }
+
+    // Creating the date picker listener for when a user selects a date
+    private DatePickerDialog.OnDateSetListener datePicker
+            = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            year_x = year;
+            month_x = monthOfYear;
+            day_x = dayOfMonth;
+            setDateString();
+        }
+    };
 }
