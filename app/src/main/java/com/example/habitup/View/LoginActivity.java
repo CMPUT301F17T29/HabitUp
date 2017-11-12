0bffbde04949ec82ebb8f798213255d1a067735b
Jeff Cho <mail@jeffcho.com>
2017-11-11 17:01:49 -0700

    Refactor LoginActivity logic, validated for empty input, user not exists, user exists.

--- app/src/main/java/com/example/habitup/View/LoginActivity.java
+++ app/src/main/java/com/example/habitup/View/LoginActivity.java
@@ -16,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.habitup.Controller.ElasticSearchController;
+import com.example.habitup.Controller.HabitUpApplication;
 import com.example.habitup.Model.UserAccount;
 import com.example.habitup.R;
 
@@ -42,34 +43,53 @@ public class LoginActivity extends AppCompatActivity {
         imageView.setImageResource(R.drawable.habitup);
 
         // get button
-        Button logInButton = (Button) findViewById(R.id.login_button);
+        final Button logInButton = (Button) findViewById(R.id.login_button);
         Button signUpButton = (Button) findViewById(R.id.link_signup);
-
-        // Transfer EditText to string
-        logInName = ((EditText) findViewById(R.id.login_edit)).getText().toString();
 
         logInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkUserExist(logInName) && checkInternet()) {
-                    Toast.makeText(getApplicationContext(),
-                            logInName + " is now logged in.",
-                            Toast.LENGTH_SHORT).show();
 
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
+                // Transfer EditText to string
+                logInName = ((EditText) findViewById(R.id.login_edit)).getText().toString().trim();
+
+                if (logInName.isEmpty()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter a username.",
+                            Toast.LENGTH_SHORT).show();
 
                 } else if (!checkInternet()) {
                     Toast.makeText(getApplicationContext(),
                             "Error: No connection to the internet.",
-                        Toast.LENGTH_SHORT).show();
+                            Toast.LENGTH_SHORT).show();
 
                 } else {
-                    Toast.makeText(getApplicationContext(),
-                            "Error: username not found.",
-                            Toast.LENGTH_SHORT).show();
-                }
 
+                    // Login Successful - set current user
+                    UserAccount loggedInUser = null;
+                    ElasticSearchController.GetUser getUser = new ElasticSearchController.GetUser();
+                    getUser.execute(logInName);
+                    try {
+                        loggedInUser = getUser.get().get(0);
+                    } catch (Exception e) {
+                        Log.i("HabitUpDEBUG", "LogIn - could not get user " + logInName);
+                    }
+
+                    if (loggedInUser != null) {
+
+                        HabitUpApplication.setCurrentUser(loggedInUser);
+
+                        Toast.makeText(getApplicationContext(), logInName + " is now logged in.",
+                                Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                "Error: username not found.", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
