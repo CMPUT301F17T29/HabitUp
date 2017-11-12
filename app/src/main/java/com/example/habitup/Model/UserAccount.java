58573c58c1fd2d8d7a456374d3b67276e02a00ca
tyheise <tyler.heise@gmail.com>
2017-11-11 18:08:37 -0700

    Added some functionality to encode Bitmaps for ES

--- app/src/main/java/com/example/habitup/Model/UserAccount.java
+++ app/src/main/java/com/example/habitup/Model/UserAccount.java
@@ -1,10 +1,13 @@
 package com.example.habitup.Model;
 
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.util.Log;
 
 import com.example.habitup.Controller.ElasticSearchController;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * @author gojeffcho
@@ -21,6 +24,7 @@ public class UserAccount {
     private String username;
     private String realname;
     private Bitmap photo;
+    private String encodedPhoto;
     private int level;
     private int XP;
     private int XPtoNext;
@@ -40,6 +44,7 @@ public class UserAccount {
         this.setUsername(username);
         this.setRealname(realname);
         this.setPhoto(photo);
+        this.setEncodedPhoto(photo);
 
         level = 1;
         XP = 0;
@@ -70,6 +75,11 @@ public class UserAccount {
      * @return Image if associated, null if not
      */
     public Bitmap getPhoto() { return this.photo; }
+
+    public Bitmap getPhotoFromEncode() {
+        byte [] decodedBytes = Base64.decode(this.encodedPhoto, 0);
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
 
     /**
      * Gets Attribute object owned by UserAccount
@@ -209,6 +219,17 @@ public class UserAccount {
     public void setPhoto(Bitmap photo) {
         if (photo != null) {
             this.photo = photo;
+        }
+    }
+
+    public void setEncodedPhoto(Bitmap photo) {
+
+        if (photo != null) {
+
+            ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOS);
+
+            this.encodedPhoto = Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
         }
     }
 
