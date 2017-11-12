9bd84b9462ca369ffb9ecc7138df5ccb33e7c432
alido8592 <alido@ualberta.ca>
2017-11-12 16:53:16 -0700

    minor commit

--- app/src/main/java/com/example/habitup/View/ViewHabitEventActivity.java
+++ app/src/main/java/com/example/habitup/View/ViewHabitEventActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
