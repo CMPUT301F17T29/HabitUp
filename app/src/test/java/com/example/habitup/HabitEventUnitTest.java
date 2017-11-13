c03f60d5e07a9d3d9aac2aadfdede78f9d279a1a
acsyl <shuyang2@ualberta.ca>
2017-11-12 21:07:01 -0700

    updated HabitEventList unittest

--- app/src/test/java/com/example/habitup/HabitEventUnitTest.java
+++ app/src/test/java/com/example/habitup/HabitEventUnitTest.java
@@ -10,7 +10,9 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 
-
+/**
+ * Update by Shuyang on 2017-11-12.
+ */
 
 public class HabitEventUnitTest extends ActivityInstrumentationTestCase2 {
 
