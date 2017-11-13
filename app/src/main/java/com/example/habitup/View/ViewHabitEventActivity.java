377680501e532ad62279c9485747a40b7bf9d2b0
alido8592 <alido@ualberta.ca>
2017-11-13 02:14:09 -0700

    habit filter and comment search working with tap
    clicking edittext field also now resets habit filter and the field

--- app/src/main/java/com/example/habitup/View/ViewHabitEventActivity.java
+++ app/src/main/java/com/example/habitup/View/ViewHabitEventActivity.java
@@ -13,6 +13,7 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -52,13 +53,14 @@ public class ViewHabitEventActivity extends BaseActivity {
     private EventListAdapter eventAdapter;
     private EditText commentFilter;
     private ArrayList<Habit> habitTypes;
+    private Spinner habitSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("HabitUpDEBUG", "ViewHabitEventActivity onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
-
+        events = new ArrayList<HabitEvent>();
         eventListView = (RecyclerView) findViewById(R.id.event_list);
         eventListView.setHasFixedSize(true);
 
@@ -77,7 +79,8 @@ public class ViewHabitEventActivity extends BaseActivity {
         ElasticSearchController.GetHabitEventsByUidTask getHabitEvents = new ElasticSearchController.GetHabitEventsByUidTask();
         getHabitEvents.execute(HabitUpApplication.getCurrentUIDAsString());
         try {
-            events = getHabitEvents.get();
+            events.clear();
+            events.addAll(getHabitEvents.get());
         } catch (Exception e) {
             Log.i("HabitUpDEBUG", "ViewHabitEvent - Couldn't get HabitEvents");
         }
@@ -116,6 +119,8 @@ public class ViewHabitEventActivity extends BaseActivity {
         });
 
         commentFilter = (EditText) findViewById(R.id.filter_comment);
+        commentFilter.setText("");
+        commentFilter.setOnClickListener(spinCl);
         commentFilter.setOnKeyListener(cFilter);
 
         // Date format for displaying event date
@@ -131,7 +136,7 @@ public class ViewHabitEventActivity extends BaseActivity {
         }
 
         // Set up habit type filter spinner
-        Spinner habitSpinner = (Spinner) findViewById(R.id.filter_habit_spinner);
+        habitSpinner = (Spinner) findViewById(R.id.filter_habit_spinner);
         ArrayAdapter<String> habitAdapter = new ArrayAdapter<String>(this, R.layout.habit_spinner);
         habitAdapter.add("All Habit Types");
 
@@ -156,6 +161,7 @@ public class ViewHabitEventActivity extends BaseActivity {
         habitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                commentFilter.setText("");
                 refreshEvents(); // refreshes through ES re-get
                 ArrayList<HabitEvent> filtList = new ArrayList<HabitEvent>();
                 filtList.clear();
@@ -170,8 +176,6 @@ public class ViewHabitEventActivity extends BaseActivity {
                         }
                     }
                 }
-                eventAdapter = new EventListAdapter(context, filtList);
-                eventListView.setAdapter(eventAdapter);
                 Collections.sort(filtList, new Comparator<HabitEvent>() {
                     @Override
                     public int compare(HabitEvent e1, HabitEvent e2) {
@@ -179,6 +183,8 @@ public class ViewHabitEventActivity extends BaseActivity {
                     }
                 });
                 Collections.reverse(filtList);
+                events.clear();
+                events.addAll(filtList);
                 eventAdapter.notifyDataSetChanged();
             }
 
@@ -282,14 +288,14 @@ public class ViewHabitEventActivity extends BaseActivity {
         ElasticSearchController.GetHabitEventsByUidTask getHabitEvents = new ElasticSearchController.GetHabitEventsByUidTask();
         getHabitEvents.execute(HabitUpApplication.getCurrentUIDAsString());
         try {
-            events = getHabitEvents.get();
+            events.clear();
+            events.addAll(getHabitEvents.get());
         } catch (Exception e) {
             Log.i("HabitUpDEBUG", "ViewHabitEvent - Couldn't get HabitEvents");
         }
     }
 
     private ArrayList<HabitEvent> filterComSort(String text, ArrayList<HabitEvent> initEvents) {
-        refreshEvents(); // refreshes through ES re-get
         ArrayList<HabitEvent> filtListCom = new ArrayList<HabitEvent>();
         filtListCom.clear();
         if (text.length()==0){
@@ -316,16 +322,25 @@ public class ViewHabitEventActivity extends BaseActivity {
         @Override
         public boolean onKey(View view, int i, KeyEvent keyEvent) {
             if ((keyEvent.getAction() == KeyEvent.ACTION_UP) && (i == KeyEvent.KEYCODE_ENTER)) {
+                InputMethodManager methodMan = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                methodMan.toggleSoftInput(0, 0);
                 String text = commentFilter.getText().toString().toLowerCase(Locale.getDefault());
+                refreshEvents(); // refreshes through ES re-get
                 ArrayList<HabitEvent> filtEvents = filterComSort(text,events);
-                eventAdapter = new EventListAdapter(context, filtEvents);
-                eventListView.setAdapter(eventAdapter);
+                events.clear();
+                events.addAll(filtEvents);
                 eventAdapter.notifyDataSetChanged();
             }
             return true;
         }
     };
 
+    EditText.OnClickListener spinCl = new EditText.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            habitSpinner.setSelection(0);
+        }
+    };
 
 }
 
