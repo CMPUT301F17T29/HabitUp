7f7fbb69c77584dfae000648febdd97b0c412722
sharibarboza <barbozashari@gmail.com>
2017-11-27 14:53:25 -0700

    Add map to drawer

--- app/src/main/java/com/example/habitup/View/MapsActivity.java
+++ app/src/main/java/com/example/habitup/View/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.habitup.View;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 
 import com.example.habitup.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,6 +20,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
